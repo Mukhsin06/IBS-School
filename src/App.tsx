ec6d920ad388a6fbcb0490c0a97680d70c4499eb
Mukhsin06 <emailingiz@example.com>
2025-01-07 17:11:10 +0500

    mmmm

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Img from"./assets/react.svg"
+
+function App() {
+    return (
+        <div className="w-full h-screen">
+            <img src={Img} alt=""/>
+        </div>
+    );
+}
+
+export default App;
